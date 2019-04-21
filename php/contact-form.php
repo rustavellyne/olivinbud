c829725c756a3d902b55a9d6d3d81e727d77efd6
Alex Chernyakov <rustavellyne@gmail.com>
2019-04-21 12:47:18 +0300

    edit colors on contacts

--- php/contact-form.php
+++ php/contact-form.php
@@ -3,51 +3,44 @@ if($_POST)
 {
 
     $name = $_POST['tp-username'];
-    $to_email       = "aligamaan@gmail.com"; //Recipient email, Replace with own email here
-    $subject        = " New Message Received from  ".$name; // the title of message 
+    $to_email       = "rustavellyne@meta.ua"; //Recipient email, Replace with own email here
+    $subject        = " Новое сообщение от пользователя  ".$name; // the title of message
     $user_email          = $_POST['tp-email'];  // user email 
     $phone_number   = $_POST['tp-phone']; // user phone number 
     $company   = $_POST['tp-company']; // user phone number 
     $message   = $_POST['tp-message'];  // message 
    
-   $message_body;
+   $message_body = '';
    
     //additional php validation
     if(!empty($to_email) && !empty($user_email) && !empty($message)){ // check If feild not empty  
         // prepare email body text
-$message_body .= "Name: "; // you can chage the text .
-$message_body .= $name;
-$message_body .= "\n";
-$message_body .="Phone Number :"; // you can chage the text .
-$message_body .=$phone_number;
-$message_body .="\n";
-$message_body .="Company :"; // you can chage the text .
-$message_body .=$company;
-$message_body .="\n";
-$message_body .= "Email: "; // you can chage the text .
-$message_body .= $user_email;
-$message_body .= "\n";
- 
-$message_body .= "Message: "; // you can chage the text .
-$message_body .= $message;
-$message_body .= "\n";
+        $message_body .= "Name: "; // you can chage the text .
+        $message_body .= $name;
+        $message_body .= "\n";
+        $message_body .="Phone Number :"; // you can chage the text .
+        $message_body .=$phone_number;
+        $message_body .="\n";
+        $message_body .="Company :"; // you can chage the text .
+        $message_body .=$company;
+        $message_body .="\n";
+        $message_body .= "Email: "; // you can chage the text .
+        $message_body .= $user_email;
+        $message_body .= "\n";
+
+        $message_body .= "Message: "; // you can chage the text .
+        $message_body .= $message;
+        $message_body .= "\n";
       
         
- // Send Mail
-   $sendmessage = mail($to_email,$subject, $message_body, "From:".$user_email);
+        // Send Mail
+        $sendmessage = mail($to_email,$subject, $message_body, "From:".$user_email);
         if(!$sendmessage){
-            
             echo 'Could not send mail! Please try agaiin .';
-        } 
-        else{
+        } else {
               echo "Hi ".$name." Thank you for your email ";
-          }
-    }
-    else{
+        }
+    } else {
           echo "You Must fill all required feild .";
     }
 }
-
-
-?>
-
